d7b69576f018470c01c9386b2e8cb834775c66c4
maxi <31918166@itbeltran.com.ar>
2024-04-26 19:41:21 -0300

    mas arreglos

--- api-Anuncios/anuncios.sql
+++ api-Anuncios/anuncios.sql
@@ -3,6 +3,10 @@ CREATE TABLE anuncios (
     titulo VARCHAR(50) NOT NULL,
     descripcion TEXT NOT NULL,
     imagen VARCHAR (255) NOT NULL,
+    carrera VARCHAR (255) NOT NULL,
+    anio VARCHAR (255) NOT NULL,
+    comision VARCHAR (255) NOT NULL,
+    estado VARCHAR (255) NOT NULL,
     fechaDesde VARCHAR(255) UNIQUE NOT NULL,
     fechaHasta VARCHAR(255) NOT NULL
 );
